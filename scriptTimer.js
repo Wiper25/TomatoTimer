b75692688643891a0f3b743ed0d1e645573f4c0c
Фаниль Ганеев <wiper996100@gmail.com>
2021-02-14 15:02:59 +0600

    Сlose to completion

--- scriptTimer.js
+++ scriptTimer.js
@@ -1,12 +1,16 @@
+
 const circle = document.querySelector('.progress_ring_circle')
 const timeId = document.getElementById('time')
 const radius = circle.r.baseVal.value
 const circumference = 8 * Math.PI * radius
 
+
 let pomodoroBlock = document.getElementById("pomodoroblockId")
 let shorkBreak = document.getElementById("shorkBreakId")
 let longBreak = document.getElementById("longBreakId")
 let betweenBreak = document.getElementById("numberPomodoroId")
+const errorValue = document.getElementById("errorValue")
+
 
 let minutes
 let timeTotel
@@ -26,12 +30,17 @@ document.getElementById('btnStopId').style.display = 'none'
 document.getElementById('btnPauseId').style.display = 'none'
 
 document.getElementById('btnSaveId').addEventListener('click', () => {
-    minutes = pomodoroBlock.value - 1
-    timeId.innerHTML = minutes + ':' + seconds
-    shork = shorkBreak.value - 1
-    long = longBreak.value - 1
-    between = betweenBreak.value
-    menuSettingsId.style.top = '-1000px'
+    if (pomodoroBlock.value < 5 || shorkBreak.value < 5 || longBreak.value < 5 || betweenBreak.value < 4 && pomodoroBlock.value > 25 || shorkBreak.value > 25 || longBreak.value > 25 || betweenBreak.value > 15) {
+        errorValue.style.opacity = '1'
+    } else {
+        minutes = pomodoroBlock.value - 1
+        timeId.innerHTML = minutes + ':' + seconds
+        shork = shorkBreak.value - 1
+        long = longBreak.value - 1
+        between = betweenBreak.value
+        menuSettingsId.style.top = '-1000px'
+    }
+
 })
 
 document.getElementById("settingsBtnId").addEventListener('click', () => {
@@ -46,7 +55,7 @@ document.getElementById('btnStartId').addEventListener('click', () => {
     if (minutes == undefined) {
         alert('Error')
     } else {
-        funStartPomodoro(between)
+        funStartPomodoro()
         document.getElementById('btnStartId').style.display = 'none'
         document.getElementById('btnPauseId').style.display = 'block'
     }
@@ -56,24 +65,46 @@ document.getElementById('btnPauseId').addEventListener('click', () => {
     document.getElementById('btnPauseId').style.display = 'none'
     document.getElementById('btnStopId').style.display = 'block'
     document.getElementById('btnContinueId').style.display = 'block'
+    clearInterval(stopPomodoroTimer)
+    clearInterval(stopSmaillBreakTimer)
+    clearInterval(stopBigTimer)
 })
 
 document.getElementById('btnStopId').addEventListener('click', () => {
-    clearInterval(stopTimer)
     timeId.innerHTML = '- -' + ' ' + ':' + ' ' + '- -'
+    minutes = undefined
+    setProgress(0)
+    document.getElementById('btnPauseId').style.display = 'none'
+    document.getElementById('btnStopId').style.display = 'none'
+    document.getElementById('btnContinueId').style.display = 'none'
+    document.getElementById('btnStartId').style.display = 'block'
+    $('body').css({ 'background-image': 'linear-gradient(90deg, #F78CA0 0%, #F9748F 20.31%, #FD868C 66.67%, #FE9A8B 100%)' });
 })
 
 document.getElementById('btnContinueId').addEventListener('click', () => {
-    funStartPomodoro()
+    clearInterval(stopPomodoroTimer)
+    clearInterval(stopSmaillBreakTimer)
+    clearInterval(stopBigTimer)
 })
+
+function setProgress(percent) {
+    const offset = circumference - percent / timeTotel * circumference
+    circle.style.strokeDashoffset = offset
+}
+
+circle.style.strokeDasharray = `${circumference} ${circumference}`
+circle.style.strokeDasharray = circumference
+
 
 let i = 0
 let stopTimer
 
-function funStartPomodoro(between) {
-        timeTotel = minutes * 4
-        document.body.style.background = "black"
-        stopTimer = setInterval(() => {
+function funStartPomodoro() {
+    $('body').css({ 'background-image': 'linear-gradient(90deg, #F78CA0 0%, #F9748F 20.31%, #FD868C 66.67%, #FE9A8B 100%)' });
+    timeTotel = minutes * 4 
+    console.log(timeTotel)
+    stopPomodoroTimer = setInterval(() => {
+
             --seconds
             timeId.innerHTML = minutes + ':' + seconds
             if (seconds == 0) {
@@ -96,13 +127,14 @@ function funStartPomodoro(between) {
 
             if (minutes < 1 && seconds === 1) {
                 ++i
-                clearInterval(stopTimer)
+                clearInterval(stopPomodoroTimer)
                 minutes = pomodoroBlock.value - 1
                 timeId.innerHTML = '- -' + ' ' + ':' + ' ' + '- -'
                 if (i < 4) {
+
                     setTimeout(funStartSmaillBreak, 1000)
                 } else {
-                    setTimeout(funStartBreakBig, 1000)
+                    setTimeout(funStartBigBreak, 1000)
                 }
                 console.log(i)
             }
@@ -111,9 +143,9 @@ function funStartPomodoro(between) {
     }
 
 function funStartSmaillBreak() {
-    document.body.style.background = "blue"
-    stopTimer2 = setInterval(() => {
-        timeTotel = shork * 4
+    $('body').css({ 'background-image': 'linear-gradient(180deg, #48C6EF 0%, #6F86D6 100%)', 'linear - gradient(90deg, #F78CA0 0 %, #F9748F 20.31 %, #FD868C 66.67 %, #FE9A8B 100 %)': 'linear- gradient(0deg, #FFFFFF, #FFFFFF);' });
+    timeTotel = shork * 4
+    stopSmaillBreakTimer = setInterval(() => {
         console.log(timeTotel)
         --seconds
         timeId.innerHTML = shork + ':' + seconds
@@ -136,18 +168,21 @@ function funStartSmaillBreak() {
         }
 
         if (shork < 1 && seconds === 1) {
-            clearInterval(stopTimer2)
+            clearInterval(stopSmaillBreakTimer)
             shork = shorkBreak.value - 1
             timeId.innerHTML = '- -' + ' ' + ':' + ' ' + '- -'
             setTimeout(funStartPomodoro, 1000)
         }
+        setProgress(shork)
     }, 1)
+
 }
 
 function funStartBreakBig() {
     timeTotel = long * 4
-    document.body.style.background = "green"
-    stopTimer3 = setInterval(() => {
+    $('body').css({ 'background-image': 'linear-gradient(180deg, #48C6EF 0%, #6F86D6 100%)', 'linear - gradient(90deg, #F78CA0 0 %, #F9748F 20.31 %, #FD868C 66.67 %, #FE9A8B 100 %)': 'linear- gradient(0deg, #FFFFFF, #FFFFFF);' });
+    console.log(timeTotel)
+    stopBigTimer = setInterval(() => {
         --seconds
         timeId.innerHTML = long + ':' + seconds
 
@@ -170,7 +205,7 @@ function funStartBreakBig() {
         }
 
         if (long < 1 && seconds === 1) {
-            clearInterval(stopTimer3)
+            clearInterval(stopBigTimer)
             long = longBreak.value - 1
             timeId.innerHTML = '- -' + ' ' + ':' + ' ' + '- -'
             console.log(i)
@@ -179,10 +214,3 @@ function funStartBreakBig() {
     }, 1)
 }
 
-function setProgress(percent) {
-    const offset = circumference - percent / timeTotel * circumference
-    circle.style.strokeDashoffset = offset
-}
-
-circle.style.strokeDasharray = `${circumference} ${circumference}`
-circle.style.strokeDasharray = circumference
